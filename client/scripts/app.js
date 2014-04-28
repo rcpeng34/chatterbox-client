5bea92fbe375f8074503e03f66966bc2d910a0d7
Hack Reactor Students <rcpeng@gmail.com>
2014-04-28 15:21:19 -0700

    Adds app object with init, send, fetch, and display methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,59 @@
-// YOUR CODE HERE:
+var app = {
+  init: function() {
+    //Do something here
+    this.server = 'https://api.parse.com/1/classes/chatterbox';
+    this.fetch();
+  },
+  send: function(message) {
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      // always use this url
+      type: 'GET',
+      url: app.server,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+        console.log(data);
+        app.display(data.results);
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message');
+      }
+    });
+  },
+  display: function(msgArray) {
+    for (var i = 0; i < msgArray.length; i++) {
+      var message = msgArray[i];
+      // debugger;
+      // console.log(message);
+      var username = message.username;
+      var text = message.text;
+      var $messageNode = $('<div class = "chat" ></div>');
+      var $usernameNode = $('<div class = "username"></div>');
+      $usernameNode.text(username);
+      var $textNode = $('<div class = "text"></div>');
+      $textNode.text(text);
+      $messageNode.append($usernameNode);
+      $messageNode.append($textNode);
+      $('#messages').append($messageNode);
+    }
+  }
+};
 
+app.init();
